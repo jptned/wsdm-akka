9def3bab56e87742314fb9be72a5b43b6ede50bf
MatthiasBakker <matthias@matesie.nl>
2020-05-13 12:17:28 +0200

    Initial setup, build.sbt + webserver

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
-  "org.scalatest" %% "scalatest" % "3.1.0" % Test
+  "org.scalatest" %% "scalatest" % "3.1.0" % Test,
+  "com.typesafe.akka" %% "akka-http"   % "10.1.12",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.5",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.12"
 )
---
+++ src/main/scala/microservice/Webserver.scala
@@ -0,0 +1,34 @@
+package microservice
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object WebServer {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route = concat(
+      path("users" / "create") {
+        post {
+          complete(HttpEntity(ContentTypes.`application/json`, "1"))
+        }
+      },
+
+    )
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
