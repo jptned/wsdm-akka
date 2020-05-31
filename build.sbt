4321f38ab9adf47e3c5389bcfc6dded80767231b
Alexander Sterk <ajhsterk@gmail.com>
2020-05-31 13:01:44 +0200

    User Actor

--- build.sbt
+++ build.sbt
@@ -13,6 +13,7 @@ libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.1.0" % Test,
   "com.typesafe.akka" %% "akka-http"   % "10.1.12",
   "com.typesafe.akka" %% "akka-stream" % "2.6.5",
+  "com.typesafe.akka" %% "akka-cluster-typed" % akkaVersion,
   "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.5" % Test,
   "com.typesafe.akka" %% "akka-http-testkit" % "10.1.12" % Test,
   "com.typesafe.play" %% "play-json" % "2.8.1"
---
+++ src/main/scala/actors/User.scala
@@ -0,0 +1,125 @@
+package actors
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.cluster.ddata.Replicator._
+import akka.cluster.ddata.typed.scaladsl.DistributedData
+import akka.cluster.ddata.typed.scaladsl.Replicator.{Get, Update}
+import akka.cluster.ddata.{ReplicatedData, SelfUniqueAddress}
+import types.{UserType, UserTypeKey}
+
+import scala.concurrent.duration._
+
+object User {
+
+  sealed trait Command
+  final case class FindUser(replyTo: ActorRef[UserResponse]) extends Command
+  final case class SubtractCredit(number: Long, replyTo: ActorRef[UserResponse]) extends Command
+  final case class AddCredit(number: Long, replyTo: ActorRef[UserResponse]) extends Command
+  final case class CreateUser(replyTo: ActorRef[UserResponse]) extends Command
+
+  sealed trait UserResponse
+  final case class User(user_id: String, credit: BigInt) extends UserResponse
+  final case class Failed(reason: String) extends UserResponse
+  final case class Successful() extends UserResponse
+
+  private sealed trait InternalCommand extends Command
+  private case class InternalFindResponse(replyTo: ActorRef[UserResponse], rsp: GetResponse[UserType]) extends InternalCommand
+  private case class InternalUpdateResponse[A <: ReplicatedData](replyTo: ActorRef[UserResponse], rsp: UpdateResponse[A]) extends InternalCommand
+  private case class InternalCreateResponse(replyTo: ActorRef[UserResponse], getResponse: GetResponse[UserType]) extends InternalCommand
+
+  private val timeout = 3.seconds
+  private val readMajority = ReadMajority(timeout)
+  private val writeMajority = WriteMajority(timeout)
+
+  def apply(user_id: String): Behavior[Command] = Behaviors.setup { context =>
+    DistributedData.withReplicatorMessageAdapter[Command, UserType] { replicator =>
+      implicit val node: SelfUniqueAddress = DistributedData(context.system).selfUniqueAddress
+
+      val DataKey = UserTypeKey("user-" + user_id)
+
+      def behavior = Behaviors.receiveMessagePartial(
+        receiveFindUser
+          .orElse(reveiveSubtractCredit)
+          .orElse(receiveAddCredit)
+          .orElse(receiveCreateUser)
+          .orElse(receiveOther)
+      )
+
+      def receiveFindUser: PartialFunction[Command, Behavior[Command]] = {
+        case FindUser(replyTo) =>
+          replicator.askGet(
+            askReplyTo => Get(DataKey, readMajority, askReplyTo),
+            rsp => InternalFindResponse(replyTo, rsp))
+
+          Behaviors.same
+
+        case InternalFindResponse(replyTo, g@GetSuccess(DataKey, _)) =>
+          val user = g.get(DataKey)
+          replyTo ! User(user.user_id, user.creditValue)
+          Behaviors.same
+
+        case InternalFindResponse(replyTo, NotFound(DataKey, _)) =>
+          replyTo ! Failed("Couldn't find " + DataKey)
+          Behaviors.same
+
+        case InternalFindResponse(replyTo, GetFailure(DataKey, _)) =>
+          // ReadMajority failure, try again with local read
+          replicator.askGet(
+            askReplyTo => Get(DataKey, ReadLocal, askReplyTo),
+            rsp => InternalFindResponse(replyTo, rsp))
+
+          Behaviors.same
+      }
+
+      def reveiveSubtractCredit: PartialFunction[Command, Behavior[Command]] = {
+        case SubtractCredit(number, replyTo) =>
+          replicator.askUpdate(
+            askReplyTo => Update(DataKey, UserType.create(user_id), writeMajority, askReplyTo) {
+              user => user.decrement(number)
+            },
+            rsp => InternalUpdateResponse(replyTo, rsp))
+
+          Behaviors.same
+      }
+
+      def receiveAddCredit: PartialFunction[Command, Behavior[Command]] = {
+        case AddCredit(number, replyTo) =>
+          replicator.askUpdate(
+            askReplyTo => Update(DataKey, UserType.create(user_id), writeMajority, askReplyTo) {
+              user => user.increment(number)
+            },
+            rsp => InternalUpdateResponse(replyTo, rsp))
+
+          Behaviors.same
+      }
+
+      def receiveCreateUser: PartialFunction[Command, Behavior[Command]] = {
+        case CreateUser(replyTo) =>
+          replicator.askUpdate(
+            askReplyTo => Update(DataKey, UserType.create(user_id), writeMajority, askReplyTo) {
+              user => user
+            },
+            rsp => InternalUpdateResponse(replyTo, rsp))
+
+          Behaviors.same
+      }
+
+      def receiveOther: PartialFunction[Command, Behavior[Command]] = {
+        case InternalUpdateResponse(replyTo, _: UpdateSuccess[_]) =>
+          replyTo ! Successful()
+          Behaviors.same
+        case InternalUpdateResponse(replyTo, _: UpdateTimeout[_]) =>
+          // UpdateTimeout, will eventually be replicated
+          replyTo ! Successful()
+          Behaviors.same
+        case InternalUpdateResponse(replyTo, e: UpdateFailure[_]) =>
+          replyTo ! Failed("Failure updating " + e)
+          Behaviors.same
+      }
+
+      behavior
+    }
+  }
+
+}
