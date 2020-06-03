6d2490e190e71531916d8898e303d64131ea2ae0
Alexander Sterk <ajhsterk@gmail.com>
2020-06-03 23:50:38 +0200

    Add Delete + tests

--- src/test/scala/actors/UserTest.scala
+++ src/test/scala/actors/UserTest.scala
@@ -4,37 +4,49 @@ package actors
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import org.scalatest.wordspec.AnyWordSpecLike
 
+object IDGenerator {
+  private var i = 0
+  def getID(): String = {
+    i += 1
+    i.toString
+  }
+}
+
 class UserTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   "User" must {
     "create a user" in {
-      val user = testKit.spawn(User("1"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
     }
     
     "find a user" in {
-      val user = testKit.spawn(User("2"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.FindUser(probe.ref)
-      probe.expectMessage(User.User("2", 0))
+      probe.expectMessage(User.User(id, 0))
     }
     
     "add credit" in {
-      val user = testKit.spawn(User("3"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.AddCredit(100, probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.FindUser(probe.ref)
-      probe.expectMessage(User.User("3", 100))
+      probe.expectMessage(User.User(id, 100))
     }
 
     "subtract credit" in {
-      val user = testKit.spawn(User("4"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
@@ -43,18 +55,20 @@ class UserTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       user ! User.SubtractCredit(50, probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.FindUser(probe.ref)
-      probe.expectMessage(User.User("4", 50))
+      probe.expectMessage(User.User(id, 50))
     }
     
     "not be able to find a user which does not exist" in {
-      val user = testKit.spawn(User("DOESNOTEXIST"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.FindUser(probe.ref)
-      probe.expectMessage(User.Failed("Couldn't find " + "user-" + "DOESNOTEXIST"))
+      probe.expectMessage(User.Failed("Couldn't find " + "user-" + id))
     }
     
     "throw an error when trying to subtract more credit than user has" in {
-      val user = testKit.spawn(User("5"))
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
       val probe = testKit.createTestProbe[User.UserResponse]()
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
@@ -63,33 +77,67 @@ class UserTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       user ! User.SubtractCredit(11, probe.ref)
       probe.expectMessage(User.NotEnoughCredit())
       user ! User.FindUser(probe.ref)
-      probe.expectMessage(User.User("5", 10))
+      probe.expectMessage(User.User(id, 10))
+    }
+    
+    "delete a user" in {
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
+      val probe = testKit.createTestProbe[User.UserResponse]()
+      user ! User.CreateUser(probe.ref)
+      probe.expectMessage(User.Successful())
+      user ! User.DeleteUser(probe.ref)
+      probe.expectMessage(User.Successful())
+    }
+    
+    "not find a deleted user" in {
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
+      val probe = testKit.createTestProbe[User.UserResponse]()
+      user ! User.CreateUser(probe.ref)
+      probe.expectMessage(User.Successful())
+      user ! User.DeleteUser(probe.ref)
+      probe.expectMessage(User.Successful())
+      user ! User.FindUser(probe.ref)
+      probe.expectMessage(User.Failed("Couldn't find " + "user-" + id))
+    }
+
+    "not recreate a deleted user" in {
+      val id = IDGenerator.getID()
+      val user = testKit.spawn(User(id))
+      val probe = testKit.createTestProbe[User.UserResponse]()
+      user ! User.CreateUser(probe.ref)
+      probe.expectMessage(User.Successful())
+      user ! User.DeleteUser(probe.ref)
+      probe.expectMessage(User.Successful())
+      user ! User.CreateUser(probe.ref)
+      probe.expectMessage(User.Failed("A deleted key can not be used again: user-" + id))
     }
   }
   
-  "User" must {
-    val id = "user"
+  "Single user actor" must {
+    val id = IDGenerator.getID()
     val user = testKit.spawn(User(id))
     val probe = testKit.createTestProbe[User.UserResponse]()
     
-    "create in a single actor" in {
+    "create" in {
       user ! User.CreateUser(probe.ref)
       probe.expectMessage(User.Successful())
     }
     
-    "find in a single actor" in {
+    "find" in {
       user ! User.FindUser(probe.ref)
       probe.expectMessage(User.User(id, 0))
     }
     
-    "add credit in a single actor" in {
+    "add credit" in {
       user ! User.AddCredit(100, probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.FindUser(probe.ref)
       probe.expectMessage(User.User(id, 100))
     }
     
-    "subtract credit in a single actor" in {
+    "subtract credit" in {
       user ! User.SubtractCredit(50, probe.ref)
       probe.expectMessage(User.Successful())
       user ! User.FindUser(probe.ref)
@@ -101,6 +149,21 @@ class UserTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       probe.expectMessage(User.NotEnoughCredit())
       user ! User.FindUser(probe.ref)
       probe.expectMessage(User.User(id, 50))
+    }
+    
+    "delete an actor" in {
+      user ! User.DeleteUser(probe.ref)
+      probe.expectMessage(User.Successful())
+    }
+    
+    "be unable to find deleted" in {
+      user ! User.FindUser(probe.ref)
+      probe.expectMessage(User.Failed("Couldn't find " + "user-" + id))
+    }
+    
+    "not be able to recreate user" in {
+      user ! User.CreateUser(probe.ref)
+      probe.expectMessage(User.Failed("A deleted key can not be used again: user-" + id))
     }
   }
 }
